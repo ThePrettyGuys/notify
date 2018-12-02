83ce36b2ad5ba6fc05a0bd51d21acdc199a86634
Unknown <marcos.alvarenga@10pines.com>
2018-12-02 20:14:21 -0300

    Se quita el código  repetido en el controller de notificaciones

---
+++ config/config.js
@@ -0,0 +1,10 @@
+const SLACKSERVICESURL = "https://hooks.slack.com/services/";
+const SLACKURL = 'https://slack.com/';
+
+exports.UNQFYURL = "http://localhost:3000/";
+exports.SLACKCHANNELURL = SLACKSERVICESURL + "TCD2F8CMP/BE9HUCKQR/1iAX0d0TTcaViTV56YpUJCTa"
+exports.MUSTLOG = true;
+exports.SLACKCLIENTID= "421083284737.493576568229";
+exports.SLACKCLIENTSECRET = "367f559e5a108459414119c8e0d9747b";
+exports.SLACKOAUTHACESSURL = SLACKURL + 'api/oauth.access';
+exports.UNQFYARTISTURL = this.UNQFYURL + "api/artists";
